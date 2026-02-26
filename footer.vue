b89652c7041311d59662bc013739971ddd639361
azizbenabdessalem <azizbenabdessalem02@gmail.com>
2026-02-26 18:56:41 +0800

    Create footer.vue

---
+++ footer.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import {ref} from 'vue'
+  const copyright_year = ref('2025')
+</script>
+
+<template>
+  <footer>
+    <p>&copy; {{copyright_year}}</p>
+  </footer>
+</template>
+
+<style scoped>
+  footer {
+      position:fixed;
+      bottom:0;
+      padding-bottom:20px;
+  }
+</style>
